051b64d37654c8491394da9583c0391f9bd3d826
Madeline Stark <17558341+Madeline-Stark@users.noreply.github.com>
2023-01-04 17:16:46 -0800

    pass

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+# got error with pytest -x, had to jsut run pytest
 
 def return_evens(num_list):
-    pass
+    # returns a list of all of the even elements of a sequence of integers.
+   return [n for n in num_list if n % 2 == 0]
+   
 
 def make_exclamation(sentence_list):
+    # takes a list of sentence strings and returns a list of sentence strings with exclamation marks at the end.
+    return [s + "!" for s in sentence_list]
     pass
